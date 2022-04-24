413b3cb08b207ee82fd4f042d5fccb075d3481a4
Filip Bartosz <filip.bartosz00@gmail.com>
2022-04-24 15:06:31 +0200

    Added Workouts tab E2E tests

--- cypress/integration/workouts.ts
+++ cypress/integration/workouts.ts
@@ -1,7 +1,27 @@
 describe('Workouts', () => {
-    it('Visits the initial project page', () => {
+    it('Visits Workouts tab', () => {
       cy.visit('/workouts')
       cy.contains('New Workout')
+    
     })
-  })
+
+
+    it('Checks if selecting works', () => {
+        cy.visit('/workouts/001')
+        cy.contains('Jumping Jacks').should('have.css','color','rgb(255, 255, 255)')
+        cy.visit('/workouts/002')
+        cy.contains('Jumping Jacks').should('have.css','color','rgb(33, 37, 41)')
+        cy.contains('Wall Sit').should('have.css','color','rgb(255, 255, 255)')
+    })
+
+
+    it('Checks if images are displayed', () => {
+        cy.visit('/workouts')
+        cy.get('body').find('img').should('have.attr','src')
+        cy.visit('/workouts/001')
+        cy.get('body').find('img').should('have.attr','src')
+    })
+
+
+})
   
