1174354640d1b2d3e3e48266268cff70f80b47b3
Filip Bartosz <filip.bartosz00@gmail.com>
2022-05-29 18:09:06 +0200

    Home Page + About Us E2E tests

--- cypress/integration/workouts.ts
+++ cypress/integration/workouts.ts
@@ -1,26 +1,10 @@
 describe('Workouts', () => {
     it('Visits Workouts tab', () => {
       cy.visit('/workouts')
-      cy.contains('New Workout')
+      cy.get('app-workout-list > :nth-child(1) > .col-xs-12 > .btn').should('not.exist')
     
     })
 
-
-    it('Checks if selecting works', () => {
-        cy.visit('/workouts/001')
-        cy.contains('Jumping Jacks').should('have.css','color','rgb(255, 255, 255)')
-        cy.visit('/workouts/002')
-        cy.contains('Jumping Jacks').should('have.css','color','rgb(33, 37, 41)')
-        cy.contains('Wall Sit').should('have.css','color','rgb(255, 255, 255)')
-    })
-
-
-    it('Checks if images are displayed', () => {
-        cy.visit('/workouts')
-        cy.get('body').find('img').should('have.attr','src')
-        cy.visit('/workouts/001')
-        cy.get('body').find('img').should('have.attr','src')
-    })
 
 
 })
