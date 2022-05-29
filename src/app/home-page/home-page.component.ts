1a99c35149fc5839b17c3e9c2baf1ff3fddcb891
Filip Bartosz <filip.bartosz00@gmail.com>
2022-05-29 16:09:16 +0200

    Home page views auth added what closes #27

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
-
+import { Subscription } from 'rxjs/internal/Subscription';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../auth/user.model';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  logged = true;
-  userName = "TestUser";
-  constructor() { }
+  isAuthenticated = false;
+  private userSub!: Subscription;
+  email = "";
+  constructor(private authService: AuthService) {}
   
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
   ngOnInit(): void {
-
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      this.email = user.email; 
+    });
+  
   }
 }
