cbcd0671b9ffb9091cc9e1e248fabc3963258b7a
Konrad Kudlak <koqud.dev@gmail.com>
2022-05-30 18:10:08 +0200

    Authentication route guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { HistoryDetailsComponent } from "./history/history-details/history-details.component";
 import { HistoryComponent } from "./history/history.component";
 import { HomePageComponent } from "./home-page/home-page.component";
@@ -16,31 +17,63 @@ import { WorkoutEditComponent } from "./workouts/workout-edit/workout-edit.compo
 import { WorkoutStartComponent } from "./workouts/workout-start/workout-start.component";
 import { WorkoutsComponent } from "./workouts/workouts.component";
 
-const appRoutes : Routes = [
-    {path: '', component: HomePageComponent},
-    {path: 'auth', component: AuthComponent},
-    {path: 'training', component: TrainingsComponent, children:[
-        {path: '', component: TrainingsStartComponent},
-        {path: ':id', component: TrainingsDetailComponent},
-        {path: ':id/start', component: StartNewTrainingComponent}
-    ]},
-    {path: 'workouts', component: WorkoutsComponent, children:[
-        {path: '', component: WorkoutStartComponent},
-        {path: 'new', component: WorkoutEditComponent},
-        {path: ':id', component: WorkoutDetailsComponent},
-        {path: ':id/edit', component: WorkoutEditComponent},
-    ]},
-    {path: 'history', component: HistoryComponent, children:[
-        {path: ':id', component: HistoryDetailsComponent}
-    ]},
-    {path: 'leaderboard', component:  LeaderboardComponent, children:[
-        {path: ':id', component: LeaderboardDetailsComponent},
-    ]},
-    {path: 'about-us', component: AboutComponent}
-]
+const appRoutes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "auth", component: AuthComponent },
+  {
+    path: "training",
+    canActivate: [AuthGuard],
+    component: TrainingsComponent,
+    children: [
+      { path: "", component: TrainingsStartComponent },
+      {
+        path: ":id",
+        component: TrainingsDetailComponent,
+      },
+      {
+        path: ":id/start",
+        component: StartNewTrainingComponent,
+      },
+    ],
+  },
+  {
+    path: "workouts",
+    component: WorkoutsComponent,
+    children: [
+      { path: "", component: WorkoutStartComponent },
+      {
+        path: "new",
+        canActivate: [AuthGuard],
+        component: WorkoutEditComponent,
+      },
+      {
+        path: ":id",
+        canActivate: [AuthGuard],
+        component: WorkoutDetailsComponent,
+      },
+      {
+        path: ":id/edit",
+        canActivate: [AuthGuard],
+        component: WorkoutEditComponent,
+      },
+    ],
+  },
+  {
+    path: "history",
+    component: HistoryComponent,
+    canActivate: [AuthGuard],
+    children: [{ path: ":id", component: HistoryDetailsComponent }],
+  },
+  {
+    path: "leaderboard",
+    component: LeaderboardComponent,
+    children: [{ path: ":id", component: LeaderboardDetailsComponent }],
+  },
+  { path: "about-us", component: AboutComponent },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule{}
+export class AppRoutingModule {}
